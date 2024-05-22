9eae7681914f8db8a56ed1643ea5918f9ccf521c
Fred Merlo <fredmerlo@gmail.com>
2024-05-22 04:58:24 -0500

    updates the html report provider

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,9 +20,24 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['playwright-html', { 
+      testFolder: 'tests',
+      title: 'Playwright Test Report',
+      project: 'GeoFoodTruck',
+      testEnvironment: 'DEV',
+      embedAssets: true,
+      embedAttachments: true,
+      outputFolder: 'playwright-html-report',
+      startServer: false,
+      consoleLog: true,
+      consoleError: true,
+      consoleTestOutput: false,
+      theme: 'light',
+    }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
