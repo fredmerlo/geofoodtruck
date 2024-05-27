fc2c0803083e4a95ef31404948c2a59709c7ade8
Fred Merlo <fredmerlo@gmail.com>
2024-05-26 23:13:40 -0500

    adds wait for map after page refresh

--- uat/pom/MapPage.ts
+++ uat/pom/MapPage.ts
@@ -104,6 +104,7 @@ export class MapPage {
 
   async pageRefresh() {
     await this.page.reload({ waitUntil: 'domcontentloaded'});
+    await this.map.waitFor();
   }
 
   async hasValueInputFindFood(search: string) {
