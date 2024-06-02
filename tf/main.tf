535bae75c76fac5199510371d3c09df4daec30c4
Fred Merlo <fredmerlo@gmail.com>
2024-06-02 01:23:27 -0500

    fixes broken file path

--- tf/main.tf
+++ tf/main.tf
@@ -271,7 +271,7 @@ resource "aws_s3_bucket_policy" "geofoodtruck_app_bucket_policy" {
 }
 
 resource "local_file" "geofoodtruck_config_json" {
-  filename     = "config.json"
+  filename     = "${var.app_build_dir}/config.json"
   content      = jsonencode({
     distribution = "${aws_cloudfront_distribution.geofoodtruck_app_distribution.domain_name}"
   })
