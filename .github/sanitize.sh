4f4d39a9885c0ab3dabe17e3f8e5dbe496c52cc1
Fred Merlo <fredmerlo@gmail.com>
2024-06-03 05:16:32 -0500

    moves the tfplan files to github folder

--- .github/sanitize.sh
+++ .github/sanitize.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# values=(
+#     "900357929763"
+#     "E263E8AHYXLO8L"
+#     "Z2FDTNDATAQYW2"
+#     "GrXGgigVGyUzWjE0ppVRMRUgR"
+#     "EOBBYJPMYN9AX"
+#     "37f7809b-ea93-46f1-9d10-4050e2957f36"
+#     "AROA5DIMDXMR6ITLNBKXN"
+#     "6c1ead8a-5e71-4e33-ae37-010dbcd3c063"
+# )
+
 values=(
     "900357929763"
-    "E263E8AHYXLO8L"
-    "Z2FDTNDATAQYW2"
     "GrXGgigVGyUzWjE0ppVRMRUgR"
-    "EOBBYJPMYN9AX"
-    "37f7809b-ea93-46f1-9d10-4050e2957f36"
-    "AROA5DIMDXMR6ITLNBKXN"
-    "6c1ead8a-5e71-4e33-ae37-010dbcd3c063"
 )
 
 input_file="../.github/tfplan.json"
