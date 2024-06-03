aefc9dcc3692f47d62c340bbcd327aa3b07d86af
Fred Merlo <fredmerlo@gmail.com>
2024-06-02 21:29:28 -0500

    adds json sanitize process

---
+++ tf/.plan/sanitize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+values=(
+    "900357929763"
+    "E263E8AHYXLO8L"
+    "Z2FDTNDATAQYW2"
+    "GrXGgigVGyUzWjE0ppVRMRUgR"
+    "EOBBYJPMYN9AX"
+    "37f7809b-ea93-46f1-9d10-4050e2957f36"
+    "AROA5DIMDXMR6ITLNBKXN"
+    "6c1ead8a-5e71-4e33-ae37-010dbcd3c063"
+)
+
+input_file="tfplan.json"
+
+for value in "${values[@]}"; do
+    sed -i "s/$value/***/g" "$input_file"
+done
