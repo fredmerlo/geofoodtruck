004cf9458d7a397d147c932724c350fc185bfc2a
Fred Merlo <fredmerlo@gmail.com>
2024-06-05 06:35:54 -0500

    refactor sanitize script

--- .github/sanitize.sh
+++ .github/sanitize.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# values=(
-#     "900357929763"
-#     "E263E8AHYXLO8L"
-#     "Z2FDTNDATAQYW2"
-#     "GrXGgigVGyUzWjE0ppVRMRUgR"
-#     "EOBBYJPMYN9AX"
-#     "37f7809b-ea93-46f1-9d10-4050e2957f36"
-#     "AROA5DIMDXMR6ITLNBKXN"
-#     "6c1ead8a-5e71-4e33-ae37-010dbcd3c063"
-# )
-
-values=(
-    "900357929763"
-    "GrXGgigVGyUzWjE0ppVRMRUgR"
-)
-
 input_file="../.github/tfplan.json"
 
-for value in "${values[@]}"; do
+for value in "$@"; do
     sed -i "s/$value/***/g" "$input_file"
 done
