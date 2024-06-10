9aef4644021200da9e64fe96156ba1000d4f61c5
Fred Merlo <fredmerlo@gmail.com>
2024-06-09 23:15:37 -0500

    adds cloudfront put bucket acl logging policy

--- tf/main.tf
+++ tf/main.tf
@@ -60,6 +60,17 @@ resource "aws_kms_key" "geofoodtruck_kms_key" {
           "kms:Decrypt"
         ],
         Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Principal = {
+          "Service": "delivery.logs.amazonaws.com"
+        },
+        Action   = [
+          "kms:GenerateDataKey*",
+          "kms:Decrypt"
+        ],
+        Resource = "*"
       }
     ]
   })
@@ -93,25 +104,34 @@ resource "aws_s3_bucket_public_access_block" "geofoodtruck_s3_bucket_public_acce
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_bucket" "geofoodtruck_log_bucket" {
-#   bucket = "geofoodtruck-log-bucket"
-# }
+resource "aws_s3_bucket" "geofoodtruck_log_bucket" {
+  bucket = "geofoodtruck-log-bucket"
+}
 
 # resource "aws_s3_bucket_acl" "geofoodtruck_log_bucket_acl" {
 #   bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
 #   acl    = "private"
 # }
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "geofoodtruck_s3_bucket_log_server_side_encryption_configuration" {
-#   bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "geofoodtruck_s3_bucket_log_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       kms_master_key_id = aws_kms_key.geofoodtruck_kms_key.arn
-#       sse_algorithm     = "aws:kms"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.geofoodtruck_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "geofoodtruck_s3_bucket_log_public_access_block" {
+  bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_cloudfront_origin_access_control" "geofoodtruck_origin_access_control" {
   name = "geofoodtruck-app-oac"
