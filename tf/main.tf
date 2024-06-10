8dd48cf5e3dff37c604742b13db6d67605f9ad17
Fred Merlo <fredmerlo@gmail.com>
2024-06-10 00:02:34 -0500

    disable public access acls

--- tf/main.tf
+++ tf/main.tf
@@ -119,14 +119,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "geofoodtruck_s3_b
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "geofoodtruck_s3_bucket_log_public_access_block" {
-#   bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
+resource "aws_s3_bucket_public_access_block" "geofoodtruck_s3_bucket_log_public_access_block" {
+  bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_ownership_controls" "geofoodtruck_s3_bucket_log_ownership_controls" {
   bucket = aws_s3_bucket.geofoodtruck_log_bucket.id
