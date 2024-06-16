2f7cceb4af77d607ec14c4ffca0806e36efdbb6b
Fred Merlo <fredmerlo@gmail.com>
2024-06-16 16:49:29 -0500

    adds azure iac environment

---
+++ tf/azure/variables.tf
@@ -0,0 +1,20 @@
+variable "app_build_dir" {
+  description = "Path to the application build directory"
+  type        = string
+  default     = "../../build"
+}
+
+locals {
+  app_build_files = fileset(var.app_build_dir, "**/**")
+
+  content_types = {
+    "html"  = "text/html"
+    "css"   = "text/css"
+    "js"    = "application/javascript"
+    "png"   = "image/png"
+    "ico"   = "image/x-icon"
+    "txt"   = "text/plain"
+    "json"  = "application/json"
+    "map"   = "application/json"
+  }
+}
