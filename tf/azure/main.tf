73576ea339cbe7f6c8b5dd7973f9b6f9e0dfc25c
Fred Merlo <fredmerlo@gmail.com>
2024-06-17 01:55:12 -0500

    add app files

--- tf/azure/main.tf
+++ tf/azure/main.tf
@@ -23,10 +23,24 @@ resource "azurerm_key_vault" "geofoodtruck_az_key_vault" {
   sku_name            = "standard"
 
   purge_protection_enabled    = true
+}
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+resource "azurerm_key_vault_access_policy" "geofoodtruck_az_key_vault_access_policy_storage" {
+  key_vault_id = azurerm_key_vault.geofoodtruck_az_key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_storage_account.geofoodtruck_az_storage_account.identity[0].principal_id
+
+    key_permissions = [
+      "Get",
+      "UnwrapKey",
+      "WrapKey"
+    ]
+}
+
+resource "azurerm_key_vault_access_policy" "geofoodtruck_az_key_vault_access_policy_identity" {
+  key_vault_id = azurerm_key_vault.geofoodtruck_az_key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
     key_permissions = [
       "Get",
@@ -45,27 +59,11 @@ resource "azurerm_key_vault" "geofoodtruck_az_key_vault" {
       "GetRotationPolicy",
       "SetRotationPolicy",
       "Update",
+      "Backup",
+      "Release",
+      "Rotate",
+      "Import",
     ]
-  }
-}
-
-resource "azurerm_key_vault_access_policy" "geofoodtruck_az_key_vault_access_policy_storage" {
-  key_vault_id = azurerm_key_vault.geofoodtruck_az_key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_storage_account.geofoodtruck_az_storage_account.identity[0].principal_id
-
-  key_permissions = [
-    "Get",
-    "Create",
-    "Delete",
-    "List",
-    "Encrypt",
-    "Decrypt",
-    "UnwrapKey",
-    "WrapKey",
-    "Purge",
-    "Update",
-  ]
 }
 
 resource "azurerm_key_vault_key" "geofoodtruck_az_key_vault_key" {
@@ -93,7 +91,8 @@ resource "azurerm_key_vault_key" "geofoodtruck_az_key_vault_key" {
   }
 
   depends_on = [
-    azurerm_key_vault_access_policy.geofoodtruck_az_key_vault_access_policy_storage
+    azurerm_key_vault_access_policy.geofoodtruck_az_key_vault_access_policy_storage,
+    azurerm_key_vault_access_policy.geofoodtruck_az_key_vault_access_policy_identity
   ]
 }
 
@@ -108,18 +107,18 @@ resource "azurerm_storage_account" "geofoodtruck_az_storage_account" {
   identity {
     type = "SystemAssigned"
   }
+
+  lifecycle {
+    ignore_changes = [
+      customer_managed_key
+    ]
+  }
 }
 
 resource "azurerm_role_assignment" "geofoodtruck_az_role_assignment_managed_id" {
   scope                = azurerm_storage_account.geofoodtruck_az_storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "geofoodtruck_az_role_assignment_storage" {
-  scope                = azurerm_storage_account.geofoodtruck_az_storage_account.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_storage_account.geofoodtruck_az_storage_account.identity[0].principal_id
 }
 
 resource "azurerm_storage_encryption_scope" "geofoodtruck_az_storage_encryption_scope" {
@@ -140,17 +139,21 @@ resource "azurerm_storage_container" "geofoodtruck_az_storage_container" {
   storage_account_name  = azurerm_storage_account.geofoodtruck_az_storage_account.name
 }
 
-# resource "azurerm_storage_blob" "app_files" {
-#   for_each = { for file in local.app_build_files : file => file }
-#   storage_account_name = azurerm_storage_account.geofoodtruck_az_storage_account.name
-#   storage_container_name = azurerm_storage_container.geofoodtruck_az_storage_container.name
-#   type         = "Block"
-#   name         = each.value
-#   source       = "${var.app_build_dir}/${each.value}"
-#   content_type = lookup(
-#     local.content_types,
-#     element(split(".", each.value), length(split(".", each.value)) - 1),
-#     "application/octet-stream"
-#   )
-#   content_md5  = filemd5("${var.app_build_dir}/${each.value}")
-# }
+resource "azurerm_storage_blob" "app_files" {
+  depends_on = [
+    azurerm_role_assignment.geofoodtruck_az_role_assignment_managed_id,
+  ]
+
+  for_each = { for file in local.app_build_files : file => file }
+  storage_account_name = azurerm_storage_account.geofoodtruck_az_storage_account.name
+  storage_container_name = azurerm_storage_container.geofoodtruck_az_storage_container.name
+  type         = "Block"
+  name         = each.value
+  source       = "${var.app_build_dir}/${each.value}"
+  content_type = lookup(
+    local.content_types,
+    element(split(".", each.value), length(split(".", each.value)) - 1),
+    "application/octet-stream"
+  )
+  content_md5  = filemd5("${var.app_build_dir}/${each.value}")
+}
