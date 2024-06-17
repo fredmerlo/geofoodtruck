8b2d311bbb712af53568d19372dbeb8bbbda6994
Fred Merlo <fredmerlo@gmail.com>
2024-06-16 22:09:58 -0500

    use azuread for storage account

--- tf/azure/main.tf
+++ tf/azure/main.tf
@@ -110,11 +110,11 @@ resource "azurerm_storage_account" "geofoodtruck_az_storage_account" {
   }
 }
 
-# resource "azurerm_role_assignment" "geofoodtruck_az_role_assignment_" {
-#   scope                = azurerm_storage_account.geofoodtruck_az_storage_account.id
-#   role_definition_name = "Storage Blob Data Contributor"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
+resource "azurerm_role_assignment" "geofoodtruck_az_role_assignment_" {
+  scope                = azurerm_storage_account.geofoodtruck_az_storage_account.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
 
 resource "azurerm_storage_encryption_scope" "geofoodtruck_az_storage_encryption_scope" {
   name               = "geofoodtruckmanagedscope"
